5b28d3afe35cf9d2101177e8d5090ed049cd6659
brayovsky <brian.muthui@andela.com>
2017-09-05 18:33:18 +0300

    [ft-150134955] Implement bugsnag transport

--- epiclogger/epictransport.py
+++ epiclogger/epictransport.py
@@ -1,5 +1,7 @@
 import os, json
 
+import bugsnag
+
 import logging
 import colorlog
 
@@ -9,6 +11,9 @@ class EpicTransport(logging.Handler):
     def __init__(self):
         """ Set logger and formatter
         """
+        self.env = self.__check_environment()
+        self.logger = logging.getLogger('epic_logger')
+        # Configure dev transport
         logging.root.setLevel(logging.DEBUG)
         # Change this to change log format
         LOG_FORMAT = "%(asctime)s %(log_color)s%(levelname)-8s%(reset)s | %(log_color)s%(message)s%(reset)s"
@@ -18,31 +23,43 @@ class EpicTransport(logging.Handler):
             'WARNING':  'yellow',
             'ERROR':    'red',
             'CRITICAL': 'red,bold',
-	    })
+        })
 
-        stream = logging.StreamHandler()
-        stream.setFormatter(epic_format)
-        self.logger = logging.getLogger('epic_logger')
+        handler = logging.StreamHandler()
+        handler.setFormatter(epic_format)
 
         # Add handler with epic format
-        self.logger.addHandler(stream)
+        self.logger.addHandler(handler)
+
+        if self.env == "prod":
+            bugsnag_api_key = os.environ.get("BUGSNAG_API_KEY") or None
+            project_root = os.environ.get("PROJECT_ROOT")
+            bugsnag.configure(api_key=bugsnag_api_key,
+                              project_root=project_root )
+
         super(EpicTransport, self).__init__()
+
 
     def emit(self, record):
         # Check production or other
         env = self.__check_environment()
-        if env != "prod":
+        if self.env != "prod":
             self.print_to_console(record)
 
-        if ((env == "prod" or env == "staging") and record.levelname == "ERROR"):
+        if ((self.env == "prod" or self.env == "staging") and record.levelname == "ERROR"):
             self.__send_to_bugsnag(record.msg)
             self.__send_to_stackdriver(record.msg)
 
-    def __send_to_bugsnag(self, full_log):
+    def __send_to_bugsnag(self, payload):
         """ Send json to bugsnag
         """
-        # Send json to bugsnag
-        self.logger.info("Sending to bugsnag")
+        # Send payload to bugsnag
+        bs_handler = bugsnag.handlers.BugsnagHandler()
+        # send only ERROR-level logs and above
+        bs_handler.setLevel(logging.ERROR)
+        prod_logger = logging.getLogger()
+        prod_logger.addHandler(bs_handler)
+        prod_logger.error(payload)
 
     def __send_to_stackdriver(self, full_log):
         """ Send json to stackdriver
