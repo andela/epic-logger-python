dd4eb4acf32235e376faa5f8a9219ee971f05a7e
brayovsky <brian.muthui@andela.com>
2017-09-05 09:53:21 +0300

    [ft-150134955] Add tests for library

---
+++ epiclogger/test.py
@@ -0,0 +1,29 @@
+import os
+
+from main import EpicLogger
+
+def test_prod_log():
+    os.environ["PY_ENV"] = "prod"
+    test_log()
+
+def test_dev_log():
+    os.environ["PY_ENV"] = "dev"
+    test_log()
+
+def test_log():
+    try:
+        1+"jehgjkg"
+    except TypeError as type_error: 
+        test = EpicLogger()
+        test.info("Here's some info")
+        test.warn("And a warning")
+
+        # Test error
+        test.error("Adding strings and integers is not clever", {"extra":"info"}, type_error)
+
+        # Test critical
+        test.error("Cannot add numbers",{ "This": "is extra info" },type_error, 'critical')
+
+# To test uncomment any environment
+test_dev_log()
+# test_prod_log()
