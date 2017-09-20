50df2e985864383e8c9e9a8ffa549c6b2702b3c5
Ikem Okonkwo <ikem.okonkwo@andela.com>
2017-09-20 14:32:09 +0100

    chore(example): add example python script using epic_logger package

---
+++ example/my_test.py
@@ -0,0 +1,40 @@
+import sys, traceback
+import json
+from datetime import datetime
+sys.path.append('epic_logger')
+from epic_logger import getLogger
+
+logger = getLogger(__name__)
+
+class Person(object):
+    def __init__(self):
+        self.name = 'codesword'
+        self.github = 'github.com/codesword'
+
+    def __str__(self):
+        return self.name +  ' + ' + self.github
+
+class Contextitem(object):
+    def __init__(self, key="author_name", value="okonkwo"):
+        self.key = key
+        self.value = value
+
+class Context(object):
+    def invocation_metadata(self):
+        return (Contextitem(), Contextitem(key='correlation_id', value='some id'))
+
+context = Context()
+def run_user_code(log):
+    log.info('info message', extra={'person': Person()})
+    log.debug('debug message', extra={ 'ttls': { 'core': 'ikem'}, 'ctx': Context()})
+    log.warning('warning message', extra={ 'sse': ['adebayo', 'james', 'thuita'] })
+    try:
+        i = 1/0
+    except:
+        print("Exception in user code:")
+        print('-'*60)
+        log.error('error message', exc_info=True, extra={ 'user': 'ikem' , 'my_time': datetime.utcnow().isoformat()})
+        log.critical('critical message', exc_info=True, extra={ 'user': 'ikem' , 'my_time': datetime.utcnow().isoformat()})
+        print('-'*60)
+
+run_user_code(logger)
