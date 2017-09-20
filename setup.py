a9c5e4ae814c565784ab3f37b1ab27a0df158dd5
Ikem Okonkwo <ikem.okonkwo@andela.com>
2017-09-20 14:14:34 +0100

    feat: setup package

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
 from setuptools import setup
-setup(url='https://github.com/andela/epic-logger-python',
-	  name='epicloggerpython'
-	)
+
+setup(name='epic_logger',
+  version='0.1',
+  description='The funniest joke in the world',
+  url='https://github.com/andela/epic-logger-python',
+  author='Ikem Okonkwo',
+  author_email='ikem.okonkwo@andela.com',
+  license='MIT',
+  packages=['epic_logger'],
+  install_requires=[
+    'python-json-logger',
+    'bugsnag',
+  ],
+  zip_safe=False)
